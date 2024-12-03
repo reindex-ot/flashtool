71291b1501a23d92a520644618d0970aa8ee7b67
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2024-12-03 15:57:14 +0900

    Create flash_all.bat

---
+++ Windows/Tetris/flash_all.bat
@@ -0,0 +1,266 @@
+@echo off
+title Nothing Phone 2a Fastboot ROM Flasher (t.me/NothingPhone2a)
+
+echo #############################################################################
+echo #                Tetris Fastboot ROM Flasher                                #
+echo #                   Developed/Tested By                                     #
+echo #  HELLBOY017, viralbanda, spike0en, PHATwalrus, arter97, AntoninoScordino  #
+echo #          [Nothing Phone (2a) Telegram Dev Team]                           #
+echo #############################################################################
+
+cd %~dp0
+
+if not exist platform-tools-latest (
+    curl -L https://dl.google.com/android/repository/platform-tools-latest-windows.zip -o platform-tools-latest.zip
+    Call :UnZipFile "%~dp0platform-tools-latest", "%~dp0platform-tools-latest.zip"
+    del /f /q platform-tools-latest.zip
+)
+
+set fastboot=.\platform-tools-latest\platform-tools\fastboot.exe
+if not exist %fastboot% (
+    echo Fastboot cannot be executed. Aborting
+    pause
+    exit
+)
+
+set boot_partitions=boot dtbo init_boot vendor_boot
+set main_partitions=odm_dlkm product system_dlkm vendor_dlkm
+set firmware_partitions=apusys ccu connsys_bt connsys_gnss connsys_wifi dpm gpueb gz lk logo mcf_ota mcupm pi_img scp spmfw sspm tee vcp
+set logical_partitions=odm_dlkm odm vendor_dlkm product vendor system_dlkm system_ext system
+set vbmeta_partitions=vbmeta vbmeta_system vbmeta_vendor
+
+echo #############################
+echo # CHECKING FASTBOOT DEVICES #
+echo #############################
+%fastboot% devices
+
+%fastboot% getvar current-slot 2>&1 | find /c "current-slot: a" > tmpFile.txt
+set /p active_slot= < tmpFile.txt
+del /f /q tmpFile.txt
+if %active_slot% equ 0 (
+    echo #############################
+    echo # CHANGING ACTIVE SLOT TO A #
+    echo #############################
+    call :SetActiveSlot
+)
+set curSlot=a
+
+echo ###################
+echo # FORMATTING DATA #
+echo ###################
+choice /m "Wipe Data?"
+if %errorlevel% equ 1 (
+    echo Please ignore "Did you mean to format this partition?" warnings.
+    call :ErasePartition userdata
+    call :ErasePartition metadata
+)
+
+echo ############################
+echo # FLASHING BOOT PARTITIONS #
+echo ############################
+choice /m "Flash images on both slots? If unsure, say N."
+if %errorlevel% equ 1 (
+    set slot=all
+) else (
+    set slot=a
+)
+
+if %slot% equ all (
+    for %%i in (%boot_partitions%) do (
+        for %%s in (a b) do (
+            call :FlashImage %%i_%%s, %%i.img
+        )
+    ) 
+) else (
+    for %%i in (%boot_partitions%) do (
+        call :FlashImage %%i_%slot%, %%i.img
+    )
+)
+
+echo #####################
+echo # FLASHING FIRMWARE #
+echo #####################
+if %slot% equ all (
+    for %%i in (%firmware_partitions%) do (
+        for %%s in (a b) do (
+            call :FlashImage %%i_%%s, %%i.img
+        )
+    ) 
+) else (
+    for %%i in (%firmware_partitions%) do (
+        call :FlashImage %%i_%slot%, %%i.img
+    )
+)
+
+:: 'preloader_raw.img' must be flashed at a different partition name
+if %slot% equ all (
+    for %%s in (a b) do (
+            call :FlashImage preloader_%%s, preloader_raw.img
+        ) 
+) else (
+    call :FlashImage preloader_%slot%, preloader_raw.img
+)
+
+echo ###################
+echo # FLASHING VBMETA #
+echo ###################
+choice /m "Disable android verified boot?, If unsure, say N. Bootloader won't be lockable if you select Y."
+if %errorlevel% equ 1 (
+    if %slot% equ all (
+        for %%i in (%vbmeta_partitions%) do (
+            for %%s in (a b) do (
+                call :FlashImage "%%i_%%s --disable-verity --disable-verification", %%i.img
+            )
+        ) 
+    ) else (
+        for %%i in (%vbmeta_partitions%) do (
+            call :FlashImage "%%i_%slot% --disable-verity --disable-verification", %%i.img
+        )
+    )
+) else (
+    set avb_enabled=1
+    if %slot% equ all (
+        for %%i in (%vbmeta_partitions%) do (
+            for %%s in (a b) do (
+                call :FlashImage "%%i_%%s", %%i.img
+            )
+        ) 
+    ) else (
+        for %%i in (%vbmeta_partitions%) do (
+            call :FlashImage "%%i_%slot%", %%i.img
+        )
+    )
+)
+
+echo ##########################             
+echo # REBOOTING TO FASTBOOTD #       
+echo ##########################
+%fastboot% reboot fastboot
+if %errorlevel% neq 0 (
+    echo Error occured while rebooting to fastbootd. Aborting
+    pause
+    exit
+)
+
+echo ###############################
+echo # FLASHING LOGICAL PARTITIONS #
+echo ###############################
+echo Flash logical partition images?
+echo If you're about to install a custom ROM that distributes its own logical partitions, say N.
+choice /m "If unsure, say Y."
+if %errorlevel% equ 1 (
+    if not exist super.img (
+        if exist super_empty.img (
+            call :WipeSuperPartition
+        ) else (
+            call :ResizeLogicalPartition
+        )
+        for %%i in (%logical_partitions%) do (
+            call :FlashImage %%i_%curSlot%, %%i.img
+        )
+    ) else (
+        call :FlashImage super, super.img
+    )
+)
+
+echo ##########################
+echo # LOCKING THE BOOTLOADER #
+echo ##########################
+if %avb_enabled% equ 1 (
+    choice /m "Lock the bootloader? If unsure, say N."
+    if %errorlevel% equ 1 (
+        %fastboot% reboot bootloader
+        if %errorlevel% neq 0 (
+            echo Error occured while rebooting to bootloader. Aborting
+            pause
+            exit
+        ) else (
+            %fastboot% flashing lock
+        )
+    )
+)
+
+echo #############
+echo # REBOOTING #
+echo #############
+choice /m "Reboot to system? If unsure, say Y."
+if %errorlevel% equ 1 (
+    %fastboot% reboot
+)
+
+echo ########
+echo # DONE #
+echo ########
+echo Stock firmware restored.
+echo You may now optionally re-lock the bootloader if you haven't disabled android verified boot.
+
+pause
+exit
+
+:UnZipFile
+if not exist "%~dp0platform-tools-latest" (
+    powershell -command "Expand-Archive -Path '%~dp0platform-tools-latest.zip' -DestinationPath '%~dp0platform-tools-latest' -Force"
+)
+exit /b
+
+:ErasePartition
+%fastboot% erase %~1
+if %errorlevel% neq 0 (
+    call :Choice "Erasing %~1 partition failed"
+)
+exit /b
+
+:SetActiveSlot
+%fastboot% --set-active=a
+if %errorlevel% neq 0 (
+    echo Error occured while switching to slot A. Aborting
+    pause
+    exit
+)
+exit /b
+
+:WipeSuperPartition
+%fastboot% wipe-super super_empty.img
+if %errorlevel% neq 0 (
+    echo Wiping super partition failed. Fallback to deleting and creating logical partitions
+    call :ResizeLogicalPartition
+)
+exit /b
+
+:ResizeLogicalPartition
+for %%i in (%logical_partitions%) do (
+    for %%s in (a b) do (
+        call :DeleteLogicalPartition %%i_%%s-cow
+        call :DeleteLogicalPartition %%i_%%s
+    )
+    call :CreateLogicalPartition %%i_%curSlot%, 1
+)
+exit /b
+
+:DeleteLogicalPartition
+%fastboot% delete-logical-partition %~1
+if %errorlevel% neq 0 (
+    call :Choice "Deleting %~1 partition failed"
+)
+exit /b
+
+:CreateLogicalPartition
+%fastboot% create-logical-partition %~1 %~2
+if %errorlevel% neq 0 (
+    call :Choice "Creating %~1 partition failed"
+)
+exit /b
+
+:FlashImage
+%fastboot% flash %~1 %~2
+if %errorlevel% neq 0 (
+    call :Choice "Flashing %~2 failed"
+)
+exit /b
+
+:Choice
+choice /m "%~1 continue? If unsure say N"
+if %errorlevel% equ 2 (
+    exit
+)
+exit /b
